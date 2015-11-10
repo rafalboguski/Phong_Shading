c60d1877431af0127c45114356c930c6bb46c9bd
rafalboguski <BoguskiRafal@gmail.com>
2015-11-10 17:33:02 +0100

    LibGdx init files

---
+++ core/src/com/mygdx/phong/PhongShading.java
@@ -0,0 +1,23 @@
+package com.mygdx.phong;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PhongShading extends ApplicationAdapter {
+
+	@Override
+	public void create () {
+
+
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	}
+}
---
+++ desktop/src/com/mygdx/phong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.phong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.phong.PhongShading;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PhongShading(), config);
+	}
+}
