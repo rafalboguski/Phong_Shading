7ea19a241c1f586ce9d5f76572c427d72d1419c3
rafalboguski <BoguskiRafal@gmail.com>
2015-11-16 20:29:33 +0100

    Adding acos generated values table, big performance gains at 4 times more memory cost

--- core/src/com/mygdx/phong/VectorMath.java
+++ core/src/com/mygdx/phong/VectorMath.java
@@ -1,7 +1,10 @@
 package com.mygdx.phong;
 
+import com.badlogic.gdx.math.FloatCounter;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
+
+import java.util.*;
 
 
 public class VectorMath {
@@ -9,7 +12,7 @@ public class VectorMath {
     // Odbija wektor vec symetrycznie do wektora normalnego
     public static Vector3 reflect(Vector3 vec, Vector3 normal) {
         normal.nor();
-        return normal.scl((float) (normal.dot(vec) * 2)).sub(vec);
+        return normal.scl(normal.dot(vec) * 2).sub(vec);
     }
 
 
@@ -19,10 +22,30 @@ public class VectorMath {
         float aNorm = (float) Math.sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
         float bNorm = (float) Math.sqrt(b.x * b.x + b.y * b.y + b.z * b.z);
 
-        float angle = (float) (Math.acos(a.dot(b) / (aNorm * bNorm)) * MathUtils.radiansToDegrees);
+        float angle = (bakedAcos(a.dot(b) / (aNorm * bNorm)) * MathUtils.radiansToDegrees);
 
         return angle;
 
     }
+
+
+    private static float bakedAcos(float value){
+
+        int vInterpolated = (int)(value*1000);
+        Float angle = backedCos.get(vInterpolated);
+
+        if(angle != null){
+            return angle;
+        }
+        else
+        {
+            angle = (float)Math.acos(value);
+            backedCos.put(vInterpolated,angle);
+            return angle;
+        }
+
+    }
+
+    private static HashMap<Integer, Float> backedCos = new HashMap<Integer, Float>() ;
 
 }
